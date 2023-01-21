647dbd7dd6b52493776cf9eb248c5058eb700509
Oleksii Mytko <mytko.oleksii@gmail.com>
2023-01-21 12:55:44 +0100

    add start

--- src/App.js
+++ src/App.js
@@ -3,15 +3,11 @@ import React from "react";
 import './App.css';
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
-
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
-
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-
-
 
 const App = (props) => {
     return (
@@ -20,6 +16,7 @@ const App = (props) => {
             <Header/>
             <Nav/>
             <div className='app-wrapper-content'>
+                <Dialogs />
 {/*
                 <Routes>
                     <Route path='/profile' element={<Profile />}/>
@@ -34,10 +31,6 @@ const App = (props) => {
 
                 <Route component={Dialogs}/>
                 <Route component={Profile}/>*/}
-            </div>
-                <Dialogs/>
-                <Profile/>
-            <div className='app-wrapper-content'>
             </div>
         </div>
 
