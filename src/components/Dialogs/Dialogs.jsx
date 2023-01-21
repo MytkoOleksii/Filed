906e8bddcd9829e74ee476b55bd3e1fa70f13205
Oleksii Mytko <mytko.oleksii@gmail.com>
2023-01-21 16:06:32 +0100

    add 2 colons dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,32 @@ import React from 'react';
 import  teg from './Dialogs.module.css'
 const Dialogs = (props) => {
     return (
-        <div >
-            Dialogs (message)
+        <div className={teg.dialogs} >
+
+            <div className={teg.dialogsItems}>
+                <div className={teg.dialog}>
+                    Dimon
+                </div>
+                <div className={teg.dialog}>
+                    Alex
+                </div>
+                <div className={teg.dialog}>
+                    Bob
+                </div>
+                <div className={teg.dialog}>
+                    Jek
+                </div>
+                <div className={teg.dialog}>
+                    Andrey
+                </div>
+
+            </div>
+            <div className={teg.messages}>
+                <div className={teg.message}>Hi</div>
+                <div className={teg.message}>Hi 2</div>
+                <div className={teg.message}>Hi 3</div>
+                <div className={teg.message}>Hi 4</div>
+            </div>
 
         </div>
     );
