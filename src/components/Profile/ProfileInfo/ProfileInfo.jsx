9b47aa3f83f0122dc97951fc905374d31ccd931c
Oleksii Mytko <mytko.oleksii@gmail.com>
2023-03-05 16:09:20 +0100

    css profileInfo

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -21,25 +21,29 @@ function ProfileInfo(props) {
                     src='https://images.pexels.com/photos/457882/pexels-photo-457882.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'/>
             </div>
             <div className={teg.descriptionBlock} key={props.profile.id}>
-               <div >
-                   <div>
-                       {props.profile.photos.large
-                    ? <img className={teg.ava} src={props.profile.photos.large}/>
-                    : <img className={teg.ava} src={avatar}/>
-                   }
-                   </div>
-                <div> <ProfileStatus status={'hello'}/> Status profile</div>
-               </div>
-                <div>
-            <h3>{props.profile.fullName}</h3>
-                <p><b>О бо мне:</b> {props.profile.aboutMe}</p>
-                <p><b>Мои соц.сети:</b>
-                    <li><a href={"https:///" + props.profile.contacts.facebook}>Facebook</a></li>
-                    <li><a href={"http:///" + props.profile.contacts.vk}>Vk</a></li>
-                    <li><a href={props.profile.contacts.twitter}>Twitter</a></li>
-                    <li><a href={"http:///"}>Instagram</a></li>
-                    <li>Youtube:</li>
-                </p>
+                <div >
+                    <div >
+                        <div className={teg.avaStatus} >
+                            <div className={teg.about}>
+                                {props.profile.photos.large
+                                    ? <img className={teg.ava} src={props.profile.photos.large}/>
+                                    : <img className={teg.ava} src={avatar}/>
+                                }
+                            </div>
+                            <div className={teg.about}><div className={teg.status}>Status profile:<ProfileStatus status={'hello'}/> </div></div>
+                        </div>
+                    </div>
+                </div>
+                <div >
+                    <h3>{props.profile.fullName}</h3>
+                    <p><b>О бо мне:</b> {props.profile.aboutMe}</p>
+                    <p><b>Мои соц.сети:</b>
+                        <li><a href={"https:///" + props.profile.contacts.facebook}>Facebook</a></li>
+                        <li><a href={"http:///" + props.profile.contacts.vk}>Vk</a></li>
+                        <li><a href={props.profile.contacts.twitter}>Twitter</a></li>
+                        <li><a href={"http:///"}>Instagram</a></li>
+                        <li>Youtube:</li>
+                    </p>
                 </div>
                 avatar + description
             </div>
--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -5,32 +5,50 @@ class ProfileStatus extends React.Component {
         editMode: false
     }
 
-    activateEditMode () {
+    activateEditMode = () => {
+        this.setState( {
+            editMode: true,
+        });
+}
+    deactivateEditMode =  () => {
+        this.setState({
+            editMode: false,
+        });
+    }
+   /* activateEditMode () {
         //Варіант 2
         this.setState( {
             editMode: true,
         });
         //Варіант 1
-       /* this.state.editMode = true;
-        this.forceUpdate();*/
-    }
-    deactivateEditMode () {
+       /!* this.state.editMode = true;
+        this.forceUpdate();*!/
+    }*/
+   /* deactivateEditMode () {
         this.setState({
             editMode: false,
         });
-    }
+    }*/
 
     render() {
         return (
             <div>
                 {!this.state.editMode &&
                     <div>
+                        <span onDoubleClick={ this.activateEditMode}>{this.props.status}</span>
+
+                        {/*
                         <span onDoubleClick={ this.activateEditMode.bind(this)}>{this.props.status}</span>
+*/}
                     </div>
                 }
                 {this.state.editMode &&
                     <div>
+                        <input autoFocus={true} onBlur={this.deactivateEditMode} value={this.props.status}/>
+
+                        {/*
                         <input autoFocus={true} onBlur={this.deactivateEditMode.bind(this)} value={this.props.status}/>
+*/}
                     </div>
                 }
             </div>
