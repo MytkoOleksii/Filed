709df1caddaaf83b33d1ddda9efed86e898c1f9b
Oleksii Mytko <mytko.oleksii@gmail.com>
2023-04-11 13:19:58 +0200

    typescript ALL

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,5 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-import SamuraiJSApp from "./App";
+import {render, screen} from '@testing-library/react';
+import SamuraiJSApp from './App';
 
 test('renders learn react link', () => {
   render(
