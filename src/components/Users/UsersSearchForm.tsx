e1eab202d6c9803d4e15e940780fa75e5f0b2070
Oleksii Mytko <mytko.oleksii@gmail.com>
2023-04-22 18:08:46 +0200

    add ant-design select user

--- src/components/Users/UsersSearchForm.tsx
+++ src/components/Users/UsersSearchForm.tsx
@@ -3,12 +3,11 @@ import React from "react";
 import {FilterUserType} from "../../redux/users-reducer";
 import {useSelector} from "react-redux";
 import {getUsersFilter} from "../../redux/users-selectors";
-import {Button, Select, Form as AntForm} from "antd";
-
-
+import {Button, Form as AntForm, Select, Space} from "antd";
 import Search from "antd/es/input/Search";
 import {SearchOutlined} from "@ant-design/icons";
 //import Item from "antd/es/list/Item";
+const { Option } = Select;
 const  {Item} = AntForm
 //----------------- type ------------------------------//
 type PropsType = {
@@ -45,6 +44,7 @@ export const UsersSearchForm: React.FC<PropsType> = React.memo((props) => {
             term: value.term,
             friend: value.friend === "null" ? null : value.friend === "true" ? true : false,
         }
+        console.log(formFilter.term)
         const onSearch = (sss: string ) => {
             console.log(sss)
         }
@@ -52,6 +52,10 @@ export const UsersSearchForm: React.FC<PropsType> = React.memo((props) => {
         props.onFilterChanged(formFilter)
         setSubmitting(false)
     };
+    const handleChange = (value: string) => {
+        console.log(`selected ${value}`);
+    };
+
 
     return (
         <div>
@@ -66,11 +70,25 @@ export const UsersSearchForm: React.FC<PropsType> = React.memo((props) => {
             >
                 {({isSubmitting}) => (
                     <Form>
-                        <Field name="friend" as="select">
+
+                        <Field  as={Select}
+                            defaultValue="null"
+                            style={{ width: 120 }}
+                            onChange={submit}
+                            options={[
+                                { value: 'null', label: 'All' },
+                                { value: 'true', label: 'Friend' },
+                                { value: 'false', label: 'unfollowed' },
+                                { value: 'disabled', label: 'Disabled', disabled: true },
+                            ]}
+                        />
+
+
+                        {/*<Field name="friend" as="select">
                             <option value="null">All</option>
                             <option value="true">Only friends</option>
                             <option value="false">Only unfollowed</option>
-                        </Field>
+                        </Field>*/}
                         <Field as={Search} name="term" placeholder="input search text" enterButton={  <Button htmlType={'submit'} type={"primary"} disabled={isSubmitting}>
                             <SearchOutlined />
                         </Button>} style={{ width: 200 }} />
